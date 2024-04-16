f375777cf8ebb4f29cb4f9fadb148fc1b4eb34c9
rej <rzioma@acm.org>
2024-04-17 00:26:46 +0200

    First simulation

--- mag/extract.tcl
+++ mag/extract.tcl
@@ -1,7 +1,10 @@
-# This commands are taken from https://github.com/mattvenn/magic-inverter/blob/main/README.md
+# Based on https://github.com/mattvenn/magic-inverter/blob/main/README.md
 
+set project [lindex $argv $argc-1]
+load $project.mag
 extract
 ext2spice lvs
 ext2spice cthresh 0
-ext2spice
-# quit
+# ext2spice
+ext2spice -o $project.spice
+quit -noprompt
